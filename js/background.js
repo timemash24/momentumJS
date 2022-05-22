f645908926b79723388d7f5324c3e1ae6f748bac
Timemash <timemash24@gmail.com>
2022-05-22 20:21:27 +0900

    Fix bgImg class to id

--- js/background.js
+++ js/background.js
@@ -1,5 +1,3 @@
-// const width = window.innerWidth;
-// const height = window.innerHeight;
 const width = 1600;
 const height = 900;
 const KEYWORD = "sky";
@@ -7,7 +5,7 @@ const KEYWORD = "sky";
 const url = `https://source.unsplash.com/${width}x${height}/?${KEYWORD}`;
 const bgImg = document.createElement("img");
 
-bgImg.classList.add("bgImg");
+bgImg.id = "bgImg";
 bgImg.src = url;
 
 document.body.appendChild(bgImg);

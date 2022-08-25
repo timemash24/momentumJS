a1df978f7bc600b2ce526186ca4908c23f4880a7
Timemash <timemash24@gmail.com>
2022-08-25 22:31:01 +0900

    Update: use async await func to call API

--- js/background.js
+++ js/background.js
@@ -2,10 +2,10 @@
 // 1. tag 입력받고 그에 따라 이미지 새로고침
 // 2. tag 입력값 저장하고 새로고침시 그에 따른 랜덤 이미지 표시
 
-const tagForm = document.querySelector("#tag-form");
-const tagInput = document.querySelector("#tag-form input");
-let keyword = "space";
-const TAG = "tag";
+const tagForm = document.querySelector('#tag-form');
+const tagInput = document.querySelector('#tag-form input');
+let keyword = 'space';
+const TAG = 'tag';
 
 if (localStorage.getItem(TAG)) {
   keyword = localStorage.getItem(TAG);
@@ -13,33 +13,41 @@ if (localStorage.getItem(TAG)) {
 }
 
 const url = `https://source.unsplash.com/1600x900/?${keyword}`;
-const windowImg = document.createElement("img");
-const bgImg = document.createElement("img");
-const imgCover = document.createElement("img");
+const windowImg = document.createElement('img');
+const bgImg = document.createElement('img');
+const imgCover = document.createElement('img');
 
 const body = document.body;
-const middleMenu = document.querySelector(".middle_menu");
+const middleMenu = document.querySelector('.middle_menu');
 
-windowImg.id = "windowImg";
-windowImg.src = "img/window.png";
+windowImg.id = 'windowImg';
+windowImg.src = 'img/window.png';
 // body.appendChild(windowImg);
 bgImg.src = url;
-bgImg.classList.add("bg_img");
-imgCover.classList.add("img_cover");
+bgImg.classList.add('bg_img');
+imgCover.classList.add('img_cover');
 
 // body.style.backgroundImage = `url(${url})`;
 body.appendChild(bgImg);
 body.appendChild(imgCover);
 
-function searchByTag(tag) {
-  fetch(`https://source.unsplash.com/1600x900/?${tag.toLowerCase()}`).then(
-    (response) => {
-      // body.style.backgroundImage = `url(${response.url})`;
-      const newBgImg = document.querySelector(".bg_img");
-      newBgImg.src = response.url;
-    }
-  );
+async function searchByTag(tag) {
+  const response = await (
+    await fetch(`https://source.unsplash.com/1600x900/?${tag.toLowerCase()}`)
+  ).json();
+  const newBgImg = document.querySelector('.bg_img');
+  newBgImg.src = response.url;
 }
+
+// function searchByTag(tag) {
+//   fetch(`https://source.unsplash.com/1600x900/?${tag.toLowerCase()}`).then(
+//     (response) => {
+//       // body.style.backgroundImage = `url(${response.url})`;
+//       const newBgImg = document.querySelector(".bg_img");
+//       newBgImg.src = response.url;
+//     }
+//   );
+// }
 
 function handleTagSubmit(event) {
   event.preventDefault();
@@ -48,7 +56,7 @@ function handleTagSubmit(event) {
   localStorage.setItem(TAG, tag);
 }
 
-tagForm.addEventListener("submit", handleTagSubmit);
+tagForm.addEventListener('submit', handleTagSubmit);
 
 // function onWindowResize() {
 //   body.style.backgroundSize = "cover";
